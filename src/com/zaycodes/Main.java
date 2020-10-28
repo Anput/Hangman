c39ddb1a4685967e50f77e5d7ed301346e6daa8e
Isiah-D <idaniels051@gmail.com>
2020-10-28 11:25:02 -0500

    Game Menu

---
+++ src/com/zaycodes/Main.java
@@ -0,0 +1,48 @@
+package com.zaycodes;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        public class Hangman{
+                //Scanner object and variables
+                Scanner input = new scanner(System.in);
+                int guessCount;
+                int correctGuesses;
+                String toPlay, guessWord;
+                String[] hangMan = new String[] {"Head","Body","L.Arm","R.Arm", "L.Leg","R.Leg"};
+
+                System.out.println("Please save me from the hangman's noose?! (0o0)");
+                 {
+                    System.out.println("Do you wish to play a game?(Muahahaha)");
+                    System.out.println("Choose: Y/y/N/n");
+                    toPlay = input.next();
+                 if (toPlay != "Y" || "y" || "N" || "n"){
+                    System.out.println("That's not an option! Try again");
+                }
+
+
+
+                //Create "menu"
+                //begin the game
+
+                //Hava a word chosen via input
+                //Store string in an array
+
+                //loop begins(?)
+                //Have player guess a char
+                //run char through array
+
+                //if char correct code
+
+                //if char incorrect code
+
+                //Give
+
+            }
+        }
+
+
+
+    }
+
